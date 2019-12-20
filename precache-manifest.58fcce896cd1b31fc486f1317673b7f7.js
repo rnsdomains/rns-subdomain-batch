6591fcebb462349c360453f55902f603ec9bcb8a
Ilan Olkies <ilanolkies@outlook.com>
2019-12-20 19:30:12 -0300

    Updates

---
+++ precache-manifest.58fcce896cd1b31fc486f1317673b7f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a53e0512a0c52b6f68f8cb16e0eef91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ae6a15a646dbecff747",
+    "url": "/static/js/2.38246544.chunk.js"
+  },
+  {
+    "revision": "61a1097d593dc13c0b0bff86a966fe84",
+    "url": "/static/js/2.38246544.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bbbc1cab67639704971",
+    "url": "/static/js/main.e219b658.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
